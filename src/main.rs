16494a5899bca0070b2e0b154afdfaa448033c94
Aidan Maskelyne <aidan@muttleyville.org>
2023-05-16 14:48:41 +0100

    Finish VGA Buffer section

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ mod vga_buffer;
 
 /// this function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+	println!("{}", info);
 	loop {}
 }
 
@@ -16,6 +17,7 @@ static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]	// don't mangle the name of the function
 pub extern "C" fn _start() -> ! {
 	println!("Hello World{}", "!");
+	panic!("Some panic message");
 	
 	// this function is the entry point, since the linker looks for a function
 	// named `_start` by default
